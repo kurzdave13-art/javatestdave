72daa42061fd22156f5fd18c9dbfff59789593c1
kurzdave13-art <kurzdave13@gmail.com>
2026-01-22 05:30:24 +0000

    fixed my prompt.

--- PAssign01a.java
+++ PAssign01a.java
@@ -5,7 +5,9 @@ public class PAssign01a {
         double priceOfBeverages = 0.50;
         double priceOfSandwiches = 2.75;
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of beverages:");
         double numBeverages = input.nextDouble();
+        System.out.println("Enter number of sandwiches:");
         double numSandwiches = input.nextDouble();
         double totalBeveragePrice = priceOfBeverages * numBeverages;
         double totalSandwichPrice = priceOfSandwiches * numSandwiches;
--- PAssign01b.java
+++ PAssign01b.java
@@ -2,7 +2,9 @@ import java.util.Scanner;
 public class PAssign01b {
     public static void main(String [] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter base value:");
         double base = input.nextDouble();
+        System.out.println("Enter height value:");
         double height = input.nextDouble();
         double area = (base * height) / 2;
         System.out.println("A triangle with base " + base + " and height " + height + " has an area of " + area + ".");
