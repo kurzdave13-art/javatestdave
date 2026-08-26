d0c62ed3b5e1bef0d13f8a50c2a1304fed2b026a
kurzdave13-art <kurzdave13@gmail.com>
2026-08-26 14:30:12 +0000

    Lab 1 programming 2

--- Lab1/WaterBottle.java
+++ Lab1/WaterBottle.java
@@ -1,28 +1,34 @@
 public class WaterBottle {
+    // declares used variables
     public double height;
     public double radius;
     public String color;
 
+    // Uses default construtor to set values to circle
     public WaterBottle(){
         this.height = 12.0;
         this.radius = 2.0;
         this.color = "green";
     }
 
+    // uses conveinence constructor to set new values
     public WaterBottle(double newHeight, double newRadius){
         this.height = newHeight;
         this.radius = newRadius;
         this.color = "green";
     }
 
+    // sets new color of waterbottle
     public void setColor(String newColor){
         this.color = newColor;
     }
 
+    // gets volume of waterbottle
     public double getVolume(){
         return Math.PI * (radius * radius) * height;
     }
 
+    // returns all the information given prior
     public String getInfo(){
         return "A " + color + " water bottle with height "+ String.format("%.2f", height) + ", radius " + String.format("%.2f", radius) + ", and volume " + String.format("%.2f", getVolume()); 
     }
--- Lab1/WaterBottleTest.java
+++ Lab1/WaterBottleTest.java
@@ -1,9 +1,12 @@
 public class WaterBottleTest {
     public static void main(String[] args){
+        // first waterbottle instance
         WaterBottle w1 = new WaterBottle();
         System.out.println(w1.getInfo());
+        // second waterbottle instance
         WaterBottle w2 = new WaterBottle(9.5, 1.5);
         System.out.println(w2.getInfo());
+        // third waterbottle instance
         WaterBottle w3 = new WaterBottle(10.0, 2.5);
         w3.setColor("blue");
         System.out.println(w3.getInfo());
